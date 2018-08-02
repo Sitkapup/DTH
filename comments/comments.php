667b71c3d293905675bd3b377b9ea78f6932d2ae
Nicko <nickotavers@gmail.com>
2018-08-03 00:38:32 +0100

    Changes

--- comments/comments.php
+++ comments/comments.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'config.php';
 include 'comments.class.php';
 $page_id = $_GET["page"];
@@ -11,12 +12,18 @@ $settings = array(
 	);
 $comments = new Comments_System($db_details, $settings);
 $comments->grabComment($page_id);
+
+if(isset($_POST['comm_submit']))
+{
+	header('Location: index.html');
+}
+
 if($comments->success)
 	echo "<div class='alert alert-success' id='comm_status'>".$comments->success."</div>";
 else if($comments->error)
 	echo "<div class='alert alert-error' id='comm_status'>".$comments->error."</div>";
 // a simple form
-echo $comments->generateForm();
+echo $comments->generateForm('comments/comments.php');
 // we show the posted comments
 echo $comments->generateComments($page_id); // we pass the page id
 ?>
